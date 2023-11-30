8084eaaf4a0e3ac9a752ece65ce76be46b13dbc0
slwtrxx <140280940+slwtrxx@users.noreply.github.com>
2023-11-30 18:40:26 +0300

    API

---
+++ database/migrations/2023_11_28_123506_create_cards_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('cards', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->bigInteger('desk_list_id')->index()->references('desk_list_id')->on('cards');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('cards');
+    }
+};
